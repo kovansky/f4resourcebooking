552d6948f32d147827a86472d8c552c11d162c6b
kovansky <root@f4dev.me>
2020-01-24 11:17:42 +0100

    feat: created empty shortcode callback

---
+++ inc/shortcodes/form/form-shortcode.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright (c) 2020. File is part of F4 Resource Booking WordPress plugin developed by F4 Developer (Stanisław
+ * Kowański - https://www.f4dev.me) for Hufiec ZHP Piaseczno (http://piaseczno.zhp.pl). F4 Resource Booking project and
+ * all it's files are released under GNU GPLv3 license. For more details look for LICENSE file.
+ */
+
+/**
+ * Callback for [f4res_form] shortcode
+ *
+ * @param array $atts    Shortcode attributes
+ * @param null  $content Shortcode inner content
+ */
+function f4res_shortcode_form_callback($atts = [], $content = null) {
+
+}
