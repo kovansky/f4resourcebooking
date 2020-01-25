502763f0de811c932ec792795b8f95eac152900c
kovansky <root@f4dev.me>
2020-01-25 09:43:54 +0100

    feat: prepared empty fieldset for every step in form

--- inc/shortcodes/form/form-shortcode.php
+++ inc/shortcodes/form/form-shortcode.php
@@ -18,6 +18,38 @@ function f4res_shortcode_form_callback($atts = [], $content = null) {
 	// Date and time
 	// Personal information
 	?>
-	<form action="<?php the_permalink() ?>" id="booking" method="post" autocomplete="on"></form>
+	<form action="<?php the_permalink() ?>" id="booking" method="post" autocomplete="on">
+		<ul class="progressbar">
+			<li class="active"><?php __('Resource type', 'f4resbook') ?></li>
+			<li><?php __('Resource', 'f4resbook') ?></li>
+			<li><?php __('Date and time', 'f4resbook') ?></li>
+			<li><?php __('Personal information', 'f4resbook') ?></li>
+		</ul>
+		
+		<fieldset>
+			<h3 class="fs-title"><?php __('Resource type', 'f4resbook') ?></h3>
+			<h4 class="fs-subtitle"><?php __('Choose the category of what do you want to book', 'f4resbook') ?></h4>
+			<!-- ToDo: Resource types list -->
+		</fieldset>
+		
+		<fieldset>
+			<h3 class="fs-title"><?php __('Resource', 'f4resbook') ?></h3>
+			<h4 class="fs-subtitle"><?php __('Choose the resource you want to book', 'f4resbook') ?></h4>
+			<!-- ToDo: Resources list -->
+		</fieldset>
+		
+		<fieldset>
+			<h3 class="fs-title"><?php __('Date and time', 'f4resbook') ?></h3>
+			<h4 class="fs-subtitle"><?php __('Choose the date and time when you want to book', 'f4resbook') ?></h4>
+			<!-- ToDo: Date & time chooser (calendar) -->
+			<!-- ToDo: Duration input -->
+		</fieldset>
+		
+		<fieldset>
+			<h3 class="fs-title"><?php __('Personal information', 'f4resbook') ?></h3>
+			<h4 class="fs-subtitle"><?php __('Provide your personal information', 'f4resbook') ?></h4>
+			<!-- ToDo: Personal information input -->
+		</fieldset>
+	</form>
 	<?php
 }
